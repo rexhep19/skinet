403564c16b56641615e671fef3a81eb6bc9a91e9
Rexhep Azizi <rexhep_sv@hotmail.com>
2023-09-15 11:01:26 +0200

    Specification added

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<StoreContext>(options =>
---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,13 @@
+using Core.Entities;
+
+namespace Core.Specification
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
